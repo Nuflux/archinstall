77c65ea6b4b7cf8e5c03286a657fdd0e83aa3001
Nüflux <58078453+Nuflux@users.noreply.github.com>
2022-11-05 02:19:13 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -Syu 7-zip alacritty anbox-git atril autoconf automake base bashtop betterdiscord-git bison blender bridge-utils caja-image-converter caja-open-terminal caja-sendto caja-share caja-wallpaper caja-xattr-tags chiaki cli-visualizer discord-canary dotnet-sdk easyeffects efibootmgr eom fakeroot firefox flex gdm gifsicle groff guilded htop inkscape intel-ucode kde-gtk-config kdenlive kdeplasma-addons kgamma5 khotkeys kwallet-pam kwrited latte-dock lib32-mangohud lmms lutris mangohud mixxx mousepad mow-git mozillavpn mozo mplayer mugshot multimc-bin nano neofetch ocs-url openasar-git oxygen parole pfetch picom-git pkgconf pluma premid-git python-spotdl qbittorrent ristretto sddm-kcm soundux spicetify-themes-git stacer ulauncher vim vlc winetricks xdelta3 yay-git yuna-appimage zram-generator 
